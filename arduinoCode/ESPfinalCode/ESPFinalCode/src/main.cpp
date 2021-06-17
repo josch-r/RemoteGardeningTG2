469c920ce1afde0c4113768874fe8e4e5c3cd955
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2021-06-17 13:46:00 +0200

    Added comments

--- arduinoCode/ESPfinalCode/ESPFinalCode/src/main.cpp
+++ arduinoCode/ESPfinalCode/ESPFinalCode/src/main.cpp
@@ -1,36 +1,37 @@
 #include <Arduino.h>
-
-#include <Arduino.h>
 //STEPPER LIBRARY
 #include <AccelStepper.h>
 #define MotorInterfaceType 4
-//RFID READER
 
+//WIFI CONNECTION
+//Wichtig: WIFIconfig.h mit Wifi credintials updaten
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include "WIFIconfig.h"
 
-//rfid reaDER
+//RFID READER
 #include <MFRC522.h>
 #define SS_PIN D1
 #define RST_PIN D3
 
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
-
+int rfidStatus = 0; //Status um zu erkennen ob eine karte gescanned wurde
 MFRC522::MIFARE_Key key; 
 
 // Init array that will store new NUID 
 byte nuidPICC[4];
 
+//WASSER PUMPE
+#define pumpPin A0
+//backend IP entsprechend eurer IP anpassen
 String backendIP = "http://192.168.1.209:3000";
-int rfidStatus = 0;
 
 //STEPPER
 int stepdir = 0;
 AccelStepper stepper1 = AccelStepper(MotorInterfaceType, D0, D1, D2, D4);
+
 void setup() {
-  // put your setup code here, to run once:
-  //STEPPER
+  //Reader initiation
   Serial.begin(115200);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
@@ -38,15 +39,21 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
+  //Stepper setup
   stepper1.setMaxSpeed(1000);
-  //calibration();
+  //calibration(); //funktion um den endschalter zu erreichen 
+  pinMode(pumpPin, OUTPUT); //PumpenPin definieren
+  //Mit WIFI verbinden
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Waiting for connection");
   while (WiFi.status() != WL_CONNECTED) { 
     delay(500);
     Serial.print(".");
   }
+  Serial.print("Sucessfully connected to "); 
+  Serial.println(WIFI_SSID); 
 }
+//Funktion zum schalte der Pumpe für den gesetzten Amount an millisekunden 
 void waterPlant(int duration){
   Serial.print("Watering plant for ");
   Serial.print(duration);
@@ -58,31 +65,23 @@ void waterPlant(int duration){
   Serial.println(digitalRead(SS_PIN));
   rfidStatus = 0;
 }
-//Pflanze in unsere Datenbank hinzufügen
+//neue Pflanze in unsere Datenbank hinzufügen
 void addNewUIDToDB(String rdifUID) {
-  
-  // Wenn eine Wifi-Verbindung besteht
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     Serial.println(rdifUID);
-    // EIn Objekt der Klasse WifiClient erzeugen
     WiFiClient client;
-    // Eine Objekt der Klasse HTTPClient erzeugen
     HTTPClient http;
-    // Das Ziel für den Post-Request definieren 
-    // Http-Request  über den WifiClient verschicken
-    // Achtung IP-Adresse vom Node-Server hinterlegen, IP-Adresse 
-    // von dem Geräte hinterlegen auf dem euer Server läuft
+    // Ziel des Post-Requests
     String url = backendIP + "/plant/register";
     http.begin(client, url);
     // Den Content-Type für den Header definieren
     http.addHeader("Content-Type", "application/json");
 
-    // Den Request verschicken, Request Payload 
-    // JSON-String: {"temperatur": "0"}
+    // JSON-String: {"uid": "031231"}
     String jsonString = "{\"uid\": \""+rdifUID+"\"}";
     int httpCode = http.POST(jsonString); 
     // Antwort: Payload
-    String payload = http.getString();                  //Get the response payload
+    String payload = http.getString();//Get the response payload
 
     // Ausgabe des Return Codes
     Serial.println(httpCode);   //Print HTTP return code
@@ -91,7 +90,6 @@ void addNewUIDToDB(String rdifUID) {
     Serial.println(payload);    //Print request response payload
 
     // Http_connection schließen
-    
     http.end();
     }
     else {
@@ -103,6 +101,7 @@ void getPlantInfoDB(String rdifUID){
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     Serial.println(rdifUID);
     HTTPClient http;
+    //Zieladresse für Get request
     String url = backendIP + "/plant/status/" + rdifUID;
     Serial.println(url);
     http.begin(url.c_str());
@@ -114,20 +113,20 @@ void getPlantInfoDB(String rdifUID){
     Serial.println(httpCode);
     if(httpCode == 200){
     Serial.println(payload); 
+    //unsere Pflanze gießen für die ermittelte Zeit
     waterPlant(payload.toInt());
-    //waterPlant(payload) for ... ms
     }
     else if(httpCode == 404) {
       addNewUIDToDB(rdifUID);
     }
-    // Ausgabe des Antwort Payloads   //Print request response payload
-    // Http_connection schließen
+    //Http_connection schließen
     http.end();
     }       
   } else {
     Serial.println("Error in WiFi connection");
   }
 }
+//Funktion um RFID UID in DezimalZahl bzw String umzuwandeln, damit es für andere Funktionen brauchbar ist
 void printDec(byte *buffer, byte bufferSize) {
   String myString;
   for (byte i = 0; i < bufferSize; i++) {
@@ -138,9 +137,11 @@ void printDec(byte *buffer, byte bufferSize) {
   Serial.println();
   Serial.print("in String: ");
   Serial.println(myString);
+  //Datenbank Abfrage nach Pflanzen Status/Gießbedarf
   getPlantInfoDB(myString);
   myString = "";
 }
+//Loop funktion um nach neuen RFID Tags zu suchen 
 void lookForNewRFIDUID(){
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
@@ -175,7 +176,9 @@ void lookForNewRFIDUID(){
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
 }
-
+void runStepper() {
+    //Do Stepper things here!
+}
 void loop() {
   // stepper1.setSpeed(400);
   //stepper1.runSpeed();
@@ -189,14 +192,3 @@ void loop() {
     //do something
   }
 }
-//Kalibrierungsfunktion die Motor an Endschalter fährt
-
-//Funktion die Schlitten fahren lässt bis ein RFID code gescannt wird, max bis 2ter Endschalter
-
-//Bei erkannter RFID: DB abfrage neue pflanze, wenn nicht Gießabfrage, danach weiterfahren in selbe richtung
-
-//Funktion neue Pflanze anlegen bei unbekanntem RFID code
-
-//Funktion Gießabfrage
-
-//Funktion um zu gießen, Motor high schalten für ? sekunden
